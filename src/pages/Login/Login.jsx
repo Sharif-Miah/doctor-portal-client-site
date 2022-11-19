1b6920e5ef72ac24992ab7205ca6601818e6872e
farhan sharif <farhansharif1410@gmail.com>
2022-11-19 08:45:54 +0530

    login form start

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
-        <div>
-            <h1>This is Login page</h1>
+        <div className='h-[800px] flex justify-center items-center'>
+            <h1 className='text-4xl'>Login</h1>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input {...register("firstName")} />
+                    <select {...register("gender")}>
+                        <option value="female">female</option>
+                        <option value="male">male</option>
+                        <option value="other">other</option>
+                    </select>
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
